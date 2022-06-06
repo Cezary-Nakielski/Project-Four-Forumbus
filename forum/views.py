666654ce3a967776ccdd8ec55c2d7250d836e4ce
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-06 20:29:36 +0000

    DEV: Create PostsList class in views.py

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class PostsList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(approved=True).order_by('-time')
+    template_name = 'index.html'
+    paginate_by = 9
 
 # Create your views here.
