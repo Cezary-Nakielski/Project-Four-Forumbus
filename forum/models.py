43cf32181c2b4184a55d8c589b6a030ba3164c62
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-06 12:34:05 +0000

    DEV: Add Post model to models.py

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="forum_posts"
+    )
+    time = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    class Meta:
+        ordering = ["-time"]
+
+    def __str__(self):
+        return self.title
