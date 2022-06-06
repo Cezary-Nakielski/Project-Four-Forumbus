18e90f371493fbffc8e9afc86091b3c9eda0144f
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-06 18:52:58 +0000

    FEAT: Add multiple functionalities to the PostAdmin model for the admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'time', 'approved')
+    list_filter = ('approved', 'time')
+    search_fields = ('title', 'body', 'creator')
+
 # Register your models here.
