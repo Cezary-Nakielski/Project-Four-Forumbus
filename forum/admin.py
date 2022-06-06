04301a9dac3430dc0bd749c3136af8726fecacf0
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-06 19:51:12 +0000

    FEAT: Remove the 'search' and 'approve from action dropdown list' functionalities from the admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -6,8 +6,3 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'time', 'approved')
     list_filter = ('approved', 'time')
-    search_fields = ('title', 'body', 'creator')
-    action = ['approve_comments']
-
-    def approve_posts(self, request, queryset):
-        queryset.update(approved=True)
