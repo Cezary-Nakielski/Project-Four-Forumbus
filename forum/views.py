f9f85cc8d5f4b1b1a8b3489e341754e8a8121483
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-13 08:48:25 +0000

    DOCS: Add comments to views.py

--- forum/views.py
+++ forum/views.py
@@ -3,12 +3,16 @@ from django.views import generic, View
 from .models import Post
 from .forms import PostForm
 
+# Class based view for displaying list of posts
+
 
 class PostsList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(approved=True).order_by('-time')
     template_name = 'index.html'
     paginate_by = 9
+
+# Class based view for displaying content of posts
 
 
 class PostContent(View):
