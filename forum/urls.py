4c6cc2ce93ed55c2d4f2319ca662317b419a477e
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-14 14:49:52 +0000

    FEAT: Add path for updating a post to urls.py. Complete functionality for updating posts

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.PostsList.as_view(), name="homepage"),
     path('create/', views.create_post, name='create'),
     path('<slug:slug>/', views.PostContent.as_view(), name='post_content'),
+    path('update/<slug:slug>/', views.UpdatePost.as_view(), name='update'),
 ]
