cc153419698b0f01952765a3457542e3eb04c91f
Cezary-Nakielski <cezarnakielski@gmail.com>
2022-06-14 04:42:10 +0000

    DEV: Create model for creation of posts

--- forum/forms.py
+++ forum/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Post
+from forum.models import Post
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'creator', 'body', ]
+        fields = ('title', 'creator', 'body', )
+
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
